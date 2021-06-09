2cb06ca859d951fa73a5d2bcf7b4cdc37c061069
Justin <jchan43@buffalo.edu>
2021-06-09 18:02:29 -0400

    added answering polls by using user input

---
+++ AnswerPolls.php
@@ -0,0 +1,21 @@
+<?php
+function polls(){
+    echo "Poll Question\n";
+    echo "Do you like to drink water\n";
+    echo "A: Yes";
+    echo "B: No";
+    $x = readline();
+
+    if($x == "A" || $x == "a"){
+        echo "You chose A\n";
+    }
+
+    elseif($x == "B" || $x == "b"){
+        echo "You chose B\n";
+    }
+    
+    else{
+        echo "Please choose A or B";
+    }
+}
+?>
