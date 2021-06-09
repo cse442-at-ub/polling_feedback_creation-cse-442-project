8553e0d29b95161e0a8675d44dc0704d437f93fc
Kevin Chen <kchen223@buffalo.edu>
2021-06-09 19:51:27 -0400

    Change DB credentials to oceanus #21

--- score.php
+++ score.php
@@ -1,10 +1,10 @@
 <?php
 $score = $_GET['score'];
 
-$dbServerName = "localhost";
-$dbUsername = "root";
-$dbPassword = "";
-$dbName = "test";
+$dbServerName = "oceanus.cse.buffalo.edu";
+$dbUsername = "kchen223";
+$dbPassword = "50277192";
+$dbName = "kchen223_db";
 
 $conn = new mysqli($dbServerName, $dbUsername, $dbPassword, $dbName);
 
@@ -13,12 +13,12 @@ if ($conn->connect_error) {
 }
 echo "Connected successfully." . "\n";
 
-$sql2 = "INSERT INTO scores (ubit, score) VALUES ('kchen223', $score) ON DUPLICATE KEY UPDATE score = $score";
+$sql = "INSERT INTO scores (ubit, score) VALUES ('kchen223', $score) ON DUPLICATE KEY UPDATE score = $score";
 
-if ($conn->query($sql2) === TRUE) {
+if ($conn->query($sql) === TRUE) {
     echo "New record created successfully. \n";
 } else {
-    echo "Error: " . $sql2 . "<br>" . $conn->error . "\n";
+    echo "Error: " . $sql . "<br>" . $conn->error . "\n";
 }
 
 $conn->close();
