3df9a6a892e390ab405e2d035780aa672e1b9fb1
Justin <jchan43@buffalo.edu>
2021-06-09 18:42:27 -0400

    added polls by user input

---
+++ AnswerPolls.php
@@ -0,0 +1,21 @@
+<?php
+function polls(){
+    echo "Poll Question1\n";
+    echo "Do you like to drink water?\n";
+    echo "A: Yes";
+    echo "B: No";
+    $x = readline();
+
+    if($x == "A" || $x == "a"){
+        echo "You chose A\n";
+    }
+
+    elseif($x == "B" || $x == "b"){
+        echo "You chose B\n";
+    }
+    
+    else{
+        echo "Please choose from A or B";
+    }
+}
+?>
