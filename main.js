8cabe456280eeb59948dbf24403d53baeba5d646
Kevin Chen <kchen223@buffalo.edu>
2021-06-14 16:25:47 -0400

    Implement UI improvements and modals using Bootstrap #20

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
 var score = document.querySelector('active')
 
 function sendScore(current) {
-    // Alert user
+    // Keep button highlighted
     if (score != null) {
         score.classList.remove('active');
     }
     score = current;
     score.classList.add('active');
-    alert("Feedback submitted: " + score.innerHTML);
     // Send score to DB
     var xmlhttp=new XMLHttpRequest();
     xmlhttp.open("GET","score.php?score=" + score.value, true);
