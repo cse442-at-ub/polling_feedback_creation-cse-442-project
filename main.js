b4d02f1bb1afd30216b6574870069b874871f744
Justin <jchan43@buffalo.edu>
2021-06-16 04:08:02 -0400

    Created Basic DB functionality #31

---
+++ main.js
@@ -0,0 +1,25 @@
+var score = document.querySelector('active')
+
+function keepHighlighted(current) {
+    // Keep button highlighted
+    if (score != null) {
+        score.classList.remove('active');
+    }
+    score = current;
+    score.classList.add('active');
+    // Send poll question to DB
+    var xmlhttp=new XMLHttpRequest();
+    xmlhttp.open("GET","PollQuestion.php?answers=" + score.value, true);
+    xmlhttp.send();
+}
+
+//function to add more answer boxes
+$(document).ready(function() {
+    $(".add-more").click(function(){
+        var html = $(".copy").html();
+        $(".after-add-more").after(html);
+    });
+    $("body").on("click",".remove",function(){ 
+        $(this).parents(".addremove").remove();
+    });
+  });
