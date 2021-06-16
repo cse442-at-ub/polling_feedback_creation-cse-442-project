99022a5353635650c80415f5644c5e2052d6a50b
Kevin Chen <potatokevin123@gmail.com>
2021-06-16 19:09:13 -0400

    Add error handling for null/invalid inputs #20

--- main.js
+++ main.js
@@ -7,10 +7,31 @@ function sendScore(current) {
     }
     score = current;
     score.classList.add('active');
-    // Send score to DB
+
     var xmlhttp=new XMLHttpRequest();
     xmlhttp.open("GET","score.php?score=" + score.value, true);
+
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState === 4) {
+            if (xmlhttp.status === 200) {
+                console.log('Request successful');
+                sendNotification(score.innerHTML);
+            } else {
+                console.log('Request failed.');
+                document.getElementById('notification').innerHTML = "<p class='text-danger'>Feedback could not be submitted.</p>"
+            }
+        }
+    }
+
     xmlhttp.send();
-    // Display notification
-    document.getElementById('notification').innerHTML = "Your feedback has been submitted: " + "<br>" + "<b>" + score.innerHTML + "</b>"
+}
+
+function sendNotification(text) {
+    if (text == undefined || text == "") {
+        document.getElementById('notification').innerHTML = "<p class='text-danger'>No response received.</p>";
+    } else if (text == "I'm lost." || text == "Just right." || text == "This is easy.") {
+        document.getElementById('notification').innerHTML = "<p class='text-success'>Your feedback has been submitted! </p>" + "<b>" + text + "</b>"
+    } else {
+        document.getElementById('notification').innerHTML = "<p class='text-danger'>Invalid feedback option: </p>" + "<b>" + text + "</b>";
+    }
 }
