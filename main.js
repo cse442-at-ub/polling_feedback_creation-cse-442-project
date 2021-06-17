284936103a4dc5669edb2b95845becb7a53929fb
Alex Cen <alexcen@buffalo.edu>
2021-06-17 01:03:42 -0400

    Removed timer, poll is closed via db now #17

--- main.js
+++ main.js
@@ -1,44 +1,46 @@
-var pollAnswer = document.querySelector('active');
+var score = document.querySelector('active')
+
+function keepHighlighted(current) {
+    // Keep button highlighted
+    if (score != null) {
+        score.classList.remove('active');
+    }
+    score = current;
+    score.classList.add('active');
+    document.getElementById('notification').innerHTML = "Your feedback has been submitted: " + "<br>" + "<b>" + score.innerHTML + "</b>"
+    // Send poll question to DB
+    var xmlhttp=new XMLHttpRequest();
+    xmlhttp.open("GET","PollQuestion.php?answers=" + score.value, true);
+    xmlhttp.send();
+}
+
+function showResults(){
+    window.location.href = './pollResults.php'
+}
+
+
 function sendPoll(current) {
+    var pollAnswer = document.querySelector('active');
     if (pollAnswer != null) {
         pollAnswer.classList.remove('active');
     }
     pollAnswer = current;
-    pollAnswer.classList.add('active');
+    responseString.innerHTML = "Your answer of " + current.value.toString() + " has been saved."
     // Send pollAnswer to DB
     var xmlhttp=new XMLHttpRequest();
     xmlhttp.open("GET","submitPoll.php?answer=" + current.value, true);
     xmlhttp.send();
 }
 
-// Timer 
-let timeElapsed = 0;
-let startTime = 30;
-let s = document.getElementById('s');
-let currTime = document.getElementById('timer');
-let results =document.getElementById('results');
-// currTime.innerHTML = startTime - timeElapsed 
 
-let countdown = setInterval(elapseTimer, 1000);
+//function to add and remove answer boxes
+// $(document).ready(function() {
+//     $(".add-more").click(function(){
+//         var html = $(".copy").html();
+//         $(".after-add-more").after(html);
+//     });
+//     $("body").on("click",".remove",function(){ 
+//         $(this).parents(".addremove").remove();
+//     });
+//   });
 
-// $("#noModalLabel").click(function(){
-//     setTimeout(function(){
-//         $("#noModalLabel").hide();
-//     },currTime.innerHTML)
-// })
-function elapseTimer() {
-    timeElapsed += 1;
-    currTime.innerHTML = startTime - timeElapsed
-    if (currTime.innerHTML == 0){
-        clearInterval(countdown)
-        timeElapsed = 0;
-        s.innerHTML = '';
-        window.location.href = './pollResults.php'
-        // document.getElementsByClassName('mx-auto')[0].style.visibility = 'hidden';
-        // if (document.getElementsByClassName('modal-backdrop fade show')[0] != undefined){
-        //     document.getElementsByClassName('modal-backdrop fade show')[0].style.visibility = 'hidden';
-        // }
-        // document.getElementsByClassName('mx-auto-2')[0].style.visibility = 'visible';
-        
-    }
-}
