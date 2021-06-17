481313097ac0aac514a7c2bb1b37795f45eb40ae
Alex Cen <alexcen@buffalo.edu>
2021-06-17 01:05:34 -0400

    Instantly shows results from db after poll closes. #17

--- pollResults.php
+++ pollResults.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -10,18 +11,11 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
     </head>
     <body>
-        <div class="mx-auto-2" style="width: 50%; text-align: center;">
-            <p class="h1">The poll has ended.</p>
-            <button type="button" class="btn btn-outline-danger btn-lg m-2" data-bs-toggle="modal" data-bs-target="#resultsModal" value="results">Show Results.</button>
-
-            <div class="modal fade" id="resultsModal" tabindex="-1" aria-labelledby="resultsModalLabel" aria-hidden="true">
-                <div class="modal-dialog modal-dialog-centered">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="resultsModalLabel">Results:</h5>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div class="modal-body" style="text-align: left;">
+        
+            <p class="h1" style="text-align:center">The poll has ended.</p>
+            <br>
+            <p class="h2" style="text-align:center; text-decoration:underline">Results:</p>
+            </div>
                         <?php
                             $dbServerName = "oceanus.cse.buffalo.edu";
                             $dbUsername = "kchen223";
@@ -40,19 +34,12 @@
                             $result = mysqli_query($conn, $sql) or die("Bad Query: $sql");
                             
                             while($row = mysqli_fetch_array($result)){
-                                echo"{$row['pollAnswer']}: {$row['NUM']} <br>";
+                                echo"<p class='h3' style='text-align:center'>{$row['pollAnswer']}: {$row['NUM']}</p><br>";
                             }
 
                             $conn->close();
                         ?>
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Close</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+      
         <script src="main.js" async defer></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
     </body>
