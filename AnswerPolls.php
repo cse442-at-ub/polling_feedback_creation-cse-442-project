fdb97caceed7b0a82c61fdff46d819f4434bd1e1
Alex Cen <alexcen@buffalo.edu>
2021-06-17 01:14:03 -0400

    Displays Q and A from DB #33

---
+++ AnswerPolls.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Poll Question Submitted</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    </head>
+    <body>
+        <div class="mx-auto" style="text-align: left;">
+            <?php
+                echo "<h1 class='m-3'>Questions and Answers Recieved!</h1>";
+
+                $question = $_REQUEST['question'];
+                $answers = $_REQUEST['answer'];
+                $answers2 = $_REQUEST['extraAnswer'];
+                $status = "open";
+                
+                $pollAnswer = $_GET["answer"];
+                $pollAnswer2 = $_GET["answer"];
+
+                $dbServerName = "oceanus.cse.buffalo.edu";
+                $dbUsername = "kchen223";
+                $dbPassword = "50277192";
+                $dbName = "kchen223_db";
+
+                $conn = new mysqli($dbServerName, $dbUsername, $dbPassword, $dbName);
+                if ($conn->connect_error) {
+                    die("Connection failed: " . $conn->connect_error . "\n");
+                }
+                
+                // if(!empty($question) || !empty($answer)){
+                //     $sql = "INSERT INTO pollquestions (question, question_answer) VALUES ('$question', '$answers', '$answers2')";
+                //     $conn->query($sql);
+                // }else{
+                //     ;
+                // }
+
+                // $sql = "INSERT INTO pollquestions (question, question_answer) VALUES ('$question', '$answers')";
+                $sql = "INSERT INTO pollquestions (question, question_answer, question_extra_answers, status) VALUES ('$question', '$answers', '$answers2', '$status')";
+                $sql = "INSERT INTO poll (ubit, pollAnswer) VALUES ('kchen223', '$pollAnswer') ON DUPLICATE KEY UPDATE pollAnswer = '$pollAnswer2'";
+
+                $conn->query($sql);
+                
+                // $sql = "SELECT question, question_answer FROM pollquestions WHERE question = '$question'";
+                $sql = "SELECT * FROM pollquestions";
+                // $sql = "SELECT * FROM pollquestions WHERE question= '$question'";
+
+                $result = $conn->query($sql);
+
+                if ($result->num_rows > 0) {
+                    while($row = $result->fetch_assoc()) {
+                        echo "<h4 class='m-3'>" . "Question ". $row["question_number"] . ": ". $row["question"] . "</h4>" . "\n";
+                        echo "<h5 class='m-3'>" . "Answers: " . $row["question_answer"] . "</h5>" . "\n";
+                        if(!empty($row["question_extra_answers"])){
+                            echo "<h5 class='m-3'>" . "Answers2: " . $row["question_extra_answers"] . "</h5>";
+                        }else{
+                            echo "\n";
+                        }
+                      }
+                } else {
+                    echo "<h4 class='m-3'>There are no questions.</h4> \n";
+                }
+
+                // header("Location: pollQuestion.html");
+            ?>
+        </div>
+    </body>
+    <script src="main.js" async defer></script>
+</html>
+
+
+
--- pollQuestion.php
+++ pollQuestion.php
@@ -1,79 +1,33 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>UB Polling</title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-    </head>
-    <body>
-        <div class="mx-auto" style="width: 50%; text-align: center;">
-                <p class="h1" id="s">Seconds left:</p>
-                <p class="h2" id="timer"></p>
-                <h1 class="m-3">Do you think Jet Fuel can melt steel beams?</h1>
-                <button type="button" class="btn btn-outline-danger btn-lg m-2" data-bs-toggle="modal" data-bs-target="#noModal" value="No" onclick=sendPoll(this)>No</button>
-                <button type="button" class="btn btn-outline-primary btn-lg m-2" data-bs-toggle="modal" data-bs-target="#tentativeModal" value="Tentative" onclick=sendPoll(this)>Tentative</button>
-                <button type="button" class="btn btn-outline-success btn-lg m-2" data-bs-toggle="modal" data-bs-target="#yesModal" value="Yes" onclick=sendPoll(this)>Yes</button>
+<?php
+$question = $_GET["question"];
+$answers = $_GET["answers"];
+// $answers2 = $_GET["answers"];
 
-            <div class="modal fade" id="noModal" tabindex="-1" aria-labelledby="noModalLabel" aria-hidden="true">
-                <div class="modal-dialog modal-dialog-centered">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="noModalLabel">Poll submitted!</h5>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div class="modal-body" style="text-align: left;">
-                            <p>Your answer has been recorded: <strong>No</strong></p>
-                            <p><small><em>You can change your answer at any time before the timer runs out.</em></small></p>
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Close</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
+$dbServerName = "oceanus.cse.buffalo.edu";
+$dbUsername = "kchen223";
+$dbPassword = "50277192";
+$dbName = "kchen223_db";
 
-            <div class="modal fade" id="tentativeModal" tabindex="-1" aria-labelledby="tentativeModalLabel" aria-hidden="true">
-                <div class="modal-dialog modal-dialog-centered">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="tentativeModalLabel">Poll submitted!</h5>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div class="modal-body" style="text-align: left;">
-                            <p>Your answer has been recorded: <strong>Tentative</strong></p>
-                            <p><small><em>You can change your answer at any time before the timer runs out.</em></small></p>
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Close</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
+$testQuestion = "Am I working?";
+$testAnswer = "Working";
+$testExtraAnswer = "Working 2";
+$status = "open";
 
-            <div class="modal fade" id="yesModal" tabindex="-1" aria-labelledby="yesModalLabel" aria-hidden="true">
-                <div class="modal-dialog modal-dialog-centered">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="yesModalLabel">Poll submitted!</h5>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div class="modal-body" style="text-align: left;">
-                            <p>Your answer has been recorded: <strong>Yes</strong></p>
-                            <p><small><em>You can change your answer at any time before the timer runs out.</em></small></p>
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Close</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+$conn = new mysqli($dbServerName, $dbUsername, $dbPassword, $dbName);
 
-        <script src="main.js" async defer></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
-    </body>
-</html>
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error . "\n");
+}
+echo "Connected successfully." . "\n";
+
+$sql = "INSERT INTO pollquestions (question, question_answer, question_extra_answers, status) VALUES ('$testQuestion', '$testAnswer', '$testExtraAnswer', '$status')";
+
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully. \n";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error . "\n";
+}
+
+$conn->close();
+?>
