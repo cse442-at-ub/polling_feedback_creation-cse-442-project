71635490ffcbb28c1ae16f71e4a4b4ba0291845c
Alex Cen <alexcen@buffalo.edu>
2021-06-22 00:16:34 -0400

    Added front-end button for instructor to open/close poll #38

---
+++ pollStatus.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+    
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Answer Poll Question</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+        <script src="main.js" type="text/javascript" async defer></script>
+    </head>
+    
+    <body>
+    <div class="mx-auto" style="width: 50%; " text-align: center;>
+
+        <h1 class="m-3" style='text-align:center;'>Poll Status Config.</h1>
+        
+        <form method="post">
+
+            <button type="submit" name="open" id="open" class="btn btn-success btn-flat"><i class="fa fa-check"> Open Poll </i></button>
+            <button type="submit" name="close" id="close" class="btn btn-danger btn-flat"><i class="fa fa-times"> Close Poll </i></button>
+
+        
+    </div>
+</body>
+</html>
