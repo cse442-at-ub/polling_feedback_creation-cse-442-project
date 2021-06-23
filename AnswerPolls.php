1a39c6b017e85c47481d668bf7b76ae209d278f9
Justin <jchan43@buffalo.edu>
2021-06-23 19:02:35 -0400

    edited code to go with changes in the DB #40

--- AnswerPolls.php
+++ AnswerPolls.php
@@ -14,7 +14,7 @@
                 echo "<h1 class='m-3'>Questions and Answers Recieved!</h1>";
 
                 $question = $_REQUEST['question'];
-                $answers = $_REQUEST['answer1'];
+                $answers1 = $_REQUEST['answer1'];
                 $answers2 = $_REQUEST['answer2'];
                 $answers3 = $_REQUEST['answer3'];
                 $answers4 = $_REQUEST['answer4'];
@@ -35,8 +35,8 @@
                 }
                 
                 if(!empty($question) || !empty($answer)){
-                    $sql = "INSERT INTO pollquestions (question, question_choice1, question_choice2, question_choice3, question_choice4, question_choice5,status) 
-                    VALUES ('$question', '$answers', '$answers2', '$answers3', '$answers4', '$answers5', '$status')";
+                    $sql = "INSERT INTO pollquestions (question, answer_choice1, answer_choice2, answer_choice3, answer_choice4, answer_choice5,status) 
+                    VALUES ('$question', '$answers1', '$answers2', '$answers3', '$answers4', '$answers5', '$status')";
                     $conn->query($sql);
                 }else{
                     ;
@@ -48,18 +48,18 @@
                 if ($result->num_rows > 0) {
                     while($row = $result->fetch_assoc()) {
                         echo "<h4 class='m-3'>" . "Question ". $row["question_number"] . ": ". $row["question"] . "</h4>" . "\n";
-                        echo "<h5 class='m-3'>" . "Choice 1: " . $row["question_choice1"] . "</h5>" . "\n";
+                        echo "<h5 class='m-3'>" . "Answer Choice 1: " . $row["question_choice1"] . "</h5>" . "\n";
                         if(!empty($row["question_choice2"]) || !empty($row["question_choice3"]) ||!empty($row["question_choice4"]) || !empty($row["question_choice5"]) ){
-                            echo "<h5 class='m-3'>" . "Choice 2: " . $row["question_choice2"] . "</h5>";
-                            echo "<h5 class='m-3'>" . "Choice 3: " . $row["question_choice3"] . "</h5>";
-                            echo "<h5 class='m-3'>" . "Choice 4: " . $row["question_choice4"] . "</h5>";
-                            echo "<h5 class='m-3'>" . "Choice 5: " . $row["question_choice5"] . "</h5>";
+                            echo "<h5 class='m-3'>" . "Answer Choice 2: " . $row["question_choice2"] . "</h5>" . "\n";
+                            echo "<h5 class='m-3'>" . "Answer Choice 3: " . $row["question_choice3"] . "</h5>" . "\n";
+                            echo "<h5 class='m-3'>" . "Answer Choice 4: " . $row["question_choice4"] . "</h5>" . "\n";
+                            echo "<h5 class='m-3'>" . "Answer Choice 5: " . $row["question_choice5"] . "</h5>" . "\n";
                         }else{
                             echo "\n";
                         }
                       }
                 } else {
-                    echo "<h4 class='m-3'>There are no questions.</h4> \n";
+                    echo "<h4 class='m-3'>There are no questions currently in the database.</h4> \n";
                 }
 
                 // header("Location: pollQuestion.html");
