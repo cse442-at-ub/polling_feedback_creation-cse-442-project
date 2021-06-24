dd382473198f56c070a0ec662a4abac412e47f16
Alex Cen <alexcen@buffalo.edu>
2021-06-24 00:16:00 -0400

    get questions from db, added sql inject security

--- answerPollQuestion.php
+++ answerPollQuestion.php
@@ -51,23 +51,34 @@
                     if ($row['status'] == 'open'){
                         echo"<p class='h3' style='text-align:center'>The poll is open. The poll will end when the instructor closes it.</p>";
                         echo "<h4 class='m-3' style = 'text-align:center; text-decoration:underline;'>" . "Question ". $row["question_number"] . ": ". $row["question"] . "</h4>" . "\n";
-                        echo '<div class="form-check">
-                        <button type="button" class="btn btn-outline-danger btn-lg m-2" value="No" onclick=sendPoll(this)>No</button>
-    
-                                </label>';
-                        // echo $row["question_answer"] . "\n";
-                        if(!empty($row["question_extra_answers"])){
-                            echo '</div>
-                                  <div class="form-check">
-                                  <button type="button" class="btn btn-outline-primary btn-lg m-2" value="Tentative" onclick=sendPoll(this)>Tentative</button>
-                                    </label>';
-    
-                            echo '</div>
-                            <div class="form-check">
-                            <button type="button" class="btn btn-outline-success btn-lg m-2" value="Yes" onclick=sendPoll(this)>Yes</button>
-            
-                            </div>';
+                        
+                        if($row["answer_choice1"]){
+                            ?>
+                            <div class="form-check"> <button type= "button" class ="btn btn-outline-primary btn-lg m-2" value = <?php echo $row["answer_choice1"] ?> onclick=sendPoll(this)> <?php echo $row["answer_choice1"]?> </button></div>
+                        <?php
                         }
+                        if($row["answer_choice2"]){
+                            ?>
+                            <div class="form-check"> <button type= "button" class ="btn btn-outline-primary btn-lg m-2" value = <?php echo $row["answer_choice2"] ?> onclick=sendPoll(this)> <?php echo $row["answer_choice2"]?> </button></div>
+                        <?php
+                        }
+                        if($row["answer_choice3"]){
+                            ?>
+                            <div class="form-check"> <button type= "button" class ="btn btn-outline-primary btn-lg m-2" value = <?php echo $row["answer_choice3"] ?> onclick=sendPoll(this)> <?php echo $row["answer_choice3"]?> </button></div>
+                        <?php
+                        }
+                        if($row["answer_choice4"]){
+                            ?>
+                            <div class="form-check"> <button type= "button" class ="btn btn-outline-primary btn-lg m-2" value = <?php echo $row["answer_choice4"] ?> onclick=sendPoll(this)> <?php echo $row["answer_choice4"]?> </button></div>
+                        <?php
+                        }
+                        if($row["answer_choice5"]){
+                            ?>
+                            <div class="form-check"> <button type= "button" class ="btn btn-outline-primary btn-lg m-2" value = <?php echo $row["answer_choice5"] ?> onclick=sendPoll(this)> <?php echo $row["answer_choice5"]?> </button></div>
+                        <?php
+                        }
+                
+                        
                     }
                     else{
                         
