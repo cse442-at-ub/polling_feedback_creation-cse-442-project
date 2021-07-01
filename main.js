c127c21bb44e39fb99dbad12962809f40673bf11
Jessiedun <47671532+Jessiedun@users.noreply.github.com>
2021-07-01 04:08:16 -0400

    removed no longer useful code

--- main.js
+++ main.js
@@ -71,16 +71,3 @@ function sendPoll(current) {
     xmlhttp.open("GET","submitPoll.php?answer=" + current.value, true);
     xmlhttp.send();
 }
-
-
-// function to add and remove answer boxes
-// $(document).ready(function() {
-//     $(".add-more").click(function(){
-//         var html = $(".copy").html();
-//         $(".after-add-more").after(html);
-//     });
-//     $("body").on("click",".remove",function(){ 
-//         $(this).parents(".addremove").remove();
-//     });
-//   });
-
