c9cc73b6037db8808c2cec07366482d359dc5027
Kevin Chen <potatokevin123@gmail.com>
2021-07-01 00:22:35 -0400

    Update SQL querys to match database changes

--- submitPoll.php
+++ submitPoll.php
@@ -1,40 +1,39 @@
 <?php
-$pollAnswer = $_GET["answer"];
-$pollAnswer2 = $_GET["answer"];
-$questionId = $_GET["id"];
+function update_poll_answer() {
+    $dbServerName = "localhost";
+    $dbUsername = "root";
+    $dbPassword = "";
+    $dbName = "test";
+    $conn = new mysqli($dbServerName, $dbUsername, $dbPassword, $dbName);
 
-$dbServerName = "localhost";
-$dbUsername = "root";
-$dbPassword = "";
-$dbName = "test";
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error . "\n");
+    }
+    echo "Connected successfully.\n";
 
-$conn = new mysqli($dbServerName, $dbUsername, $dbPassword, $dbName);
+    $poll_answer = mysqli_real_escape_string($conn, $_GET["answer"]);
+    echo $poll_answer;
+    $question_id = mysqli_real_escape_string($conn, $_GET["id"]);
+    // $pollAnswer2 = mysqli_real_escape_string($conn, $_GET["answer"]);
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error . "\n");
+    if(isset($_COOKIE['ubit'])) {
+        $ubit = mysqli_real_escape_string($conn, $_COOKIE['ubit']);
+    }
+    if(isset($_COOKIE['course'])) {
+        $course = mysqli_real_escape_string($conn, $_COOKIE['course']);
+    }
+    
+    $stmt = $conn->prepare("INSERT INTO pollresponses (ubit, course, question_id, response) VALUES (?,?,?,?) ON DUPLICATE KEY UPDATE response = ?");
+    $stmt->bind_param('ssiss', $ubit, $course, $question_id, $poll_answer, $poll_answer);
+    
+    if ($stmt->execute() === TRUE) {
+        echo "New record created successfully. \n";
+    } else {
+        echo "Error: " . $sql . "\n" . $conn->error . "\n";
+    }
+    
+    $conn->close();
 }
-echo "Connected successfully." . "\n";
 
-// Testing cookies
-$cookie_name = "ubit";
-$cookie_value = "kchen223";
-setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/");
-
-if(!isset($_COOKIE['ubit'])) {
-    echo "Cookie named '" . $cookie_name . "' is not set!";
-} else {
-    $ubit = $_COOKIE['ubit'];
-    echo "Cookie '" . $cookie_name . "' is set!<br>";
-    echo "Value is: " . $ubit;
-}
-
-$sql = "INSERT INTO poll (ubit, question_id, pollAnswer) VALUES ('$ubit', $questionId, '$pollAnswer') ON DUPLICATE KEY UPDATE pollAnswer = '$pollAnswer2'";
-
-if ($conn->query($sql) === TRUE) {
-    echo "New record created successfully. \n";
-} else {
-    echo "Error: " . $sql . "\n" . $conn->error . "\n";
-}
-
-$conn->close();
+update_poll_answer();
 ?>
