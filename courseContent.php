328ba1216e82ecf7867265e51514b446a2fd6806
Justin <jchan43@buffalo.edu>
2021-07-02 16:54:27 -0400

    fixed displaying class for student page

--- courseContent.php
+++ courseContent.php
@@ -14,15 +14,18 @@
         <div class="mx-auto text-center">
             <?php
             // Hard coding course cookie; connect with login page later
-            // $course_key = "course";
+            $course_key = "course";
             // $course_value = "CSE 442";
             // setrawcookie($course_key, $course_value, time() + (86400 * 30), "/");
             // $ubit_key = "ubit";
             // $ubit_value = "kchen223";
             // setrawcookie($ubit_key, $ubit_value, time() + (86400 * 30), "/");
             // Checking for course cookie
+            // setcookie("course", "CSE 442", time() + (86400 * 30), "/"); 
             if(isset($_COOKIE[$course_key])) {
-                echo "<h1 class='m-3 text-center'>" . htmlspecialchars($course_value) . "</h1>";
+                echo "<h1 class='m-3 text-center'>" . htmlspecialchars($_COOKIE['course']) . "</h1>";
+            }else{
+                echo "not working";
             }
             ?>
             
